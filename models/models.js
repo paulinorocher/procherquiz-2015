e50c40d0ea04427fed00864285089ddde80fa383
paulinorocher <p.rocher@hotmail.com>
2015-08-09 21:36:53 +0100

    Commentario

--- models/models.js
+++ models/models.js
@@ -35,7 +35,15 @@ var sequelize = new Sequelize(DB_name, user, pwd,
 // Importar la definicion de la tabla Quiz en quiz.js
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 
+// Importar definicion de la tabla Comment
+var comment_path = path.join(__dirname,'comment');
+var Comment = sequelize.import(comment_path);
+
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
+
 exports.Quiz = Quiz; // exportar definicion de tabla Quiz
+exports.Comment = Comment; // exportar definicion de tabla Comment
 
 // sequelize.sync() crea e inicializa tabla de preguntas en DB
 sequelize.sync().then(function() {
@@ -54,4 +62,3 @@ sequelize.sync().then(function() {
 		};
 	});
 });
-
